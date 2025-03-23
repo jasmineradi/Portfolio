95a0f890d07c0d96061b62ffa6a096c633569a22
Jasmine Radi <jasmineradii@gmail.com>
2025-03-23 17:10:47 -0500

    Learning Bootstrap

---
+++ fitness-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Jasmine Radi
+    File Name: script.js
+    Date: February 25, 2025
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+})
